b1b1ef3dfb74662718bc5e6453800bee686e98bf
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-08-23 23:09:11 +0900

    homework 20240825 ans

---
+++ _frontend/js/App.js
@@ -0,0 +1,24 @@
+const ramenContainerElement = document.getElementById('ramen-container');
+
+function callRamenFindAll() {
+    // HTTPリクエストの作成
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:8080/ramen/findAll', true);
+    request.responseType = 'json';
+
+    // レスポンスを受け取った時の処理
+    request.onload = function () {
+        const data = this.response;
+
+        for (let i = 0; i < data.length; i++) {
+            const p = document.createElement('p');
+            p.textContent = data[i].name;
+            ramenContainerElement.appendChild(p);
+        }
+    };
+
+    // リクエストの送信
+    request.send();
+}
+
+callRamenFindAll();
--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO ramen_table(name) VALUES
-                                  ("家系");
+                                  ("家系"),
+                                  ("豚骨"),
+                                  ("醤油"),
+                                  ("味噌");
