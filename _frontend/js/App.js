95887d14571d9338cd8b51ed26a2c5e83f6258c3
y.watanabe <oomori.gohan39@gmail.com>
2024-08-29 16:29:34 +0900

    レビュー依頼

--- _frontend/js/App.js
+++ _frontend/js/App.js
@@ -1,19 +1,22 @@
-function callApi() {
-    // HTTPリクエストの作成
-
-    // GET https://localhost:8080/ramen/findAll net::ERR_SSL_PROTOCOL_ERRORでdata変数内にapiのレスポンス取得できない
+function findAllApi() {
+    // GET https://localhost:8080/ramen/findAll apiのレスポンス取得
     let request = new XMLHttpRequest();
-    request.open('GET', 'https://localhost:8080/ramen/findAll', true);
+    request.open('GET', 'http://localhost:8080/ramen/findAll', true);
     request.responseType = 'json';
 
     // レスポンスを受け取った時の処理
     request.onload = function () {
         let data = this.response;
-        console.log(data);
+        let resultElement = document.getElementById("title-container");
+
+        // APIのレスポンスのnameを順番に画面にpタグで表示する
+        for (var i = 0, len = data.length; i < len; i++) {
+          resultElement.insertAdjacentHTML('beforeend', "<p>" + data[i].name + "</p>");
+        }
     };
 
     // リクエストの送信
     request.send();
 }
 
-callApi();
+findAllApi();
