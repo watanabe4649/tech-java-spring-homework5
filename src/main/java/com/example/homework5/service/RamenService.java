b3036db944f202dfdb57338dfca89256c664e0a0
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-10-04 16:27:43 +0900

    homework 20241005 ans

--- src/main/java/com/example/homework5/service/RamenService.java
+++ src/main/java/com/example/homework5/service/RamenService.java
@@ -14,5 +14,7 @@ public interface RamenService {
 
     List<Ramen> findByFilter(int minEval, int maxEval);
 
+    Page<Ramen> findByFilter(int minEval, int maxEval, Pageable pageable);
+
     void insert(Ramen ramen);
 }
