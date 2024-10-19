0b1dd8f4900a480ccfe2c518a60c7d263fdb53ac
y.watanabe <oomori.gohan39@gmail.com>
2024-10-19 19:45:53 +0900

    何も出来ませんちメートルでした

--- _frontend/js/App.js
+++ _frontend/js/App.js
@@ -4,6 +4,10 @@ function callRamenFindByFilter() {
     // URLのクエリパラメータを取得
     const params = window.location.search;
 
+//URLのキーminEval取得できない
+//const minEval = urlParams.get('minEval');
+console.log(urlParams.get('minEval'))
+
     // HTTPリクエストの作成
     const request = new XMLHttpRequest();
     request.open('GET', `http://localhost:8080/ramen/findByFilter${params}`, true);
@@ -20,6 +24,8 @@ function callRamenFindByFilter() {
         }
     };
 
+//    location.href="http://localhost:63342/homework5/_frontend/index.html?page=1&minEval=2&maxEval=4";
+//        return false;
     // リクエストの送信
     request.send();
 }
@@ -36,3 +42,44 @@ function createRamenElement(ramen) {
 }
 
 callRamenFindByFilter();
+//callRamen()
+
+//const menuButtons = document.getElementsByClassName('pagination-item-link');
+//Array.from(menuButtons).forEach((menuButton) => {
+//  menuButton.addEventListener('click', () => {
+//    const value = document.getElementsByClassName('pagination-item-link').value;
+//    window.location.href = `https://example.com?value=${value}`;
+//
+//////      url.searchParams.append("minEval", "params.get('minEval')");
+//////      url.searchParams.append("maxEval", "params.get('maxEval')");
+//console.log(params.get('maxEval'))
+//  })
+//});
+
+//    // URLを取得
+//    let url = new URL(window.location.href);
+//       console.log(url)
+//
+//    // URLSearchParamsオブジェクトを取得
+//    let params = url.searchParams;
+//
+//    url.searchParams.append("minEval", "params.get('minEval')");
+//    url.searchParams.append("maxEval", "params.get('maxEval')");
+//
+//    if( params.get('minEval') ) {
+//        params.set('username','taro');
+//        console.log(params.get('username')); // taro
+//    }
+//
+//    if( params.get('maxEval') ) {
+//        params.set('mode','view');
+//        console.log(params.getAll('mode')); // ['view']
+     //}
+//     var baseUrl = 'http://localhost:63342/homework5/_frontend/index.html?page=';
+//     var idList= [1, 2, 3, 4,5,6,7,8,9,10];
+//     var els = document.getElementsByClassName('pagination-item-link');
+//     for (var i = 0; i < idList.length; i++) {
+//     els[i].href = baseUrl +idList[i]+params.get('minEval');
+//     }
+
+//}
