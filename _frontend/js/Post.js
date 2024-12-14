b2aa03f77032de351e8744957c36fe9219d89887
y.watanabe <oomori.gohan39@gmail.com>
2024-12-14 18:05:42 +0900

    レビュー依頼

--- _frontend/js/Post.js
+++ _frontend/js/Post.js
@@ -30,4 +30,50 @@ function postRamen() {
     request.send(JSON.stringify(ramen));
 }
 
+var stars = document.getElementsByClassName("star");
+var clicked = false;
+// 参考URL https://pen-tech.hateblo.jp/entry/2022/02/24/001512
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 0; i < stars.length; i++) {
+
+    stars[i].addEventListener(
+      "mouseover",
+      () => {
+        if (!clicked) { //クリックが一度もされていないとき
+          for (let j = 0; j <= i; j++) { //星1から上にカーソルがある星まで
+            stars[j].style.color = "#f0da61"; //黄色に
+          }
+        }
+      },
+      false
+    );
+
+    stars[i].addEventListener(
+      "mouseout",
+      () => {
+        if (!clicked) { //クリックが一度もされていないとき
+          for (let j = 0; j < stars.length; j++) { //星1から星5まで
+            stars[j].style.color = "#a09a9a"; //グレーに
+          }
+        }
+      },
+      false
+    );
+
+    stars[i].addEventListener(
+      "click",
+      () => {
+        clicked = true;
+        for (let j = 0; j <= i; j++) {
+          stars[j].style.color = "#f0da61"; //黄色に
+        }
+        for (let j = i + 1; j < stars.length; j++) {
+          stars[j].style.color = "#a09a9a"; //グレーに
+        }
+      },
+      false
+    );
+  }
+});
+
 ramenPostButtonElement.addEventListener('click', postRamen);
